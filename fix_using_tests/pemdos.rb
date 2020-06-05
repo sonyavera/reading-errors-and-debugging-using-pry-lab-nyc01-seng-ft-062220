9a4a3c7d19682d50203c1a5f023c0343bc9f8ab6
Sonya Gould <sonyavgould@gmail.com>
2020-06-05 02:32:51 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -3,7 +3,8 @@ require 'pry'
 def snake_it_up(string)
   if string[0] == "s"
     snake_sound = times.10 print "s"
-    print snake_sound + string
+    print snake_sound
+    print string
     puts "hello"
   else
     string
